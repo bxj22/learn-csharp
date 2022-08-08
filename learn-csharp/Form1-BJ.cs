82efd27e0aa3e358366694aa47bbe43ceff20d1f
katr <bjohnson@katr.com>
2022-08-08 13:01:42 -0500

    Added background thread example

---
+++ learn-csharp/Form1-BJ.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learn_csharp
+{
+    public partial class Form1 : Form
+    {
+
+        private void btnBJClear_Click(object sender, EventArgs e)
+        {
+            lbBJResults.Items.Clear();
+        }
+
+        public void WriteTextSafe(string text)
+        {
+            if (lbBJResults.InvokeRequired)
+            {
+                // Call this same method but append THREAD2 to the text
+                Action safeWrite = delegate { WriteTextSafe($"{text}"); };
+                lbBJResults.Invoke(safeWrite);
+            }
+            else
+                lbBJResults.Items.Add(text);
+        }
+
+        private void DoGUIBackgroundWrite(bool useInvoke)
+        {
+            Console.WriteLine("Background thread starting");
+            try
+            {
+                for (int idx = 0; idx < 5; idx++)
+                {
+                    var msg = $"Writing from background thread: {idx}";
+                    if (useInvoke)
+                        WriteTextSafe(msg);
+                    else
+                        lbBJResults.Items.Add(msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+           Console.WriteLine("Background thread ending");
+        }
+
+        private void btnBJWriteGUI_Click(object sender, EventArgs e)
+        {
+            new Thread(() => DoGUIBackgroundWrite(rbBJWithInvoke.Checked)) { IsBackground = true }.Start();
+        }
+
+    }
+}
--- learn-csharp/Form1.Designer.cs
+++ learn-csharp/Form1.Designer.cs
@@ -34,10 +34,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tabTJ = new System.Windows.Forms.TabPage();
             this.tabBJ = new System.Windows.Forms.TabPage();
+            this.rbBJWithoutInvoke = new System.Windows.Forms.RadioButton();
+            this.rbBJWithInvoke = new System.Windows.Forms.RadioButton();
+            this.btnBJWriteGUI = new System.Windows.Forms.Button();
+            this.btnBJClear = new System.Windows.Forms.Button();
+            this.lbBJResults = new System.Windows.Forms.ListBox();
             this.tabReferences = new System.Windows.Forms.TabPage();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.tabMain.SuspendLayout();
             this.tabHome.SuspendLayout();
+            this.tabBJ.SuspendLayout();
             this.tabReferences.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -99,12 +105,69 @@
             // 
             // tabBJ
             // 
+            this.tabBJ.Controls.Add(this.rbBJWithoutInvoke);
+            this.tabBJ.Controls.Add(this.rbBJWithInvoke);
+            this.tabBJ.Controls.Add(this.btnBJWriteGUI);
+            this.tabBJ.Controls.Add(this.btnBJClear);
+            this.tabBJ.Controls.Add(this.lbBJResults);
             this.tabBJ.Location = new System.Drawing.Point(4, 22);
             this.tabBJ.Name = "tabBJ";
             this.tabBJ.Size = new System.Drawing.Size(980, 628);
             this.tabBJ.TabIndex = 2;
             this.tabBJ.Text = "BJ Stuff";
             this.tabBJ.UseVisualStyleBackColor = true;
+            // 
+            // rbBJWithoutInvoke
+            // 
+            this.rbBJWithoutInvoke.AutoSize = true;
+            this.rbBJWithoutInvoke.Location = new System.Drawing.Point(412, 172);
+            this.rbBJWithoutInvoke.Name = "rbBJWithoutInvoke";
+            this.rbBJWithoutInvoke.Size = new System.Drawing.Size(98, 17);
+            this.rbBJWithoutInvoke.TabIndex = 17;
+            this.rbBJWithoutInvoke.TabStop = true;
+            this.rbBJWithoutInvoke.Text = "Without Invoke";
+            this.rbBJWithoutInvoke.UseVisualStyleBackColor = true;
+            // 
+            // rbBJWithInvoke
+            // 
+            this.rbBJWithInvoke.AutoSize = true;
+            this.rbBJWithInvoke.Checked = true;
+            this.rbBJWithInvoke.Location = new System.Drawing.Point(313, 172);
+            this.rbBJWithInvoke.Name = "rbBJWithInvoke";
+            this.rbBJWithInvoke.Size = new System.Drawing.Size(83, 17);
+            this.rbBJWithInvoke.TabIndex = 16;
+            this.rbBJWithInvoke.TabStop = true;
+            this.rbBJWithInvoke.Text = "With Invoke";
+            this.rbBJWithInvoke.UseVisualStyleBackColor = true;
+            // 
+            // btnBJWriteGUI
+            // 
+            this.btnBJWriteGUI.Location = new System.Drawing.Point(3, 166);
+            this.btnBJWriteGUI.Name = "btnBJWriteGUI";
+            this.btnBJWriteGUI.Size = new System.Drawing.Size(289, 23);
+            this.btnBJWriteGUI.TabIndex = 15;
+            this.btnBJWriteGUI.Text = "Write to GUI from background thread";
+            this.btnBJWriteGUI.UseVisualStyleBackColor = true;
+            this.btnBJWriteGUI.Click += new System.EventHandler(this.btnBJWriteGUI_Click);
+            // 
+            // btnBJClear
+            // 
+            this.btnBJClear.Location = new System.Drawing.Point(879, 3);
+            this.btnBJClear.Name = "btnBJClear";
+            this.btnBJClear.Size = new System.Drawing.Size(75, 23);
+            this.btnBJClear.TabIndex = 14;
+            this.btnBJClear.Text = "Clear";
+            this.btnBJClear.UseVisualStyleBackColor = true;
+            this.btnBJClear.Click += new System.EventHandler(this.btnBJClear_Click);
+            // 
+            // lbBJResults
+            // 
+            this.lbBJResults.FormattingEnabled = true;
+            this.lbBJResults.Location = new System.Drawing.Point(3, 3);
+            this.lbBJResults.Name = "lbBJResults";
+            this.lbBJResults.Size = new System.Drawing.Size(869, 147);
+            this.lbBJResults.TabIndex = 13;
+            this.lbBJResults.Click += new System.EventHandler(this.btnBJClear_Click);
             // 
             // tabReferences
             // 
@@ -139,6 +202,8 @@
             this.tabMain.ResumeLayout(false);
             this.tabHome.ResumeLayout(false);
             this.tabHome.PerformLayout();
+            this.tabBJ.ResumeLayout(false);
+            this.tabBJ.PerformLayout();
             this.tabReferences.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -154,6 +219,11 @@
         private System.Windows.Forms.TabPage tabReferences;
         private System.Windows.Forms.CheckBox cbxSaveActiveTab;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RadioButton rbBJWithoutInvoke;
+        private System.Windows.Forms.RadioButton rbBJWithInvoke;
+        private System.Windows.Forms.Button btnBJWriteGUI;
+        private System.Windows.Forms.Button btnBJClear;
+        private System.Windows.Forms.ListBox lbBJResults;
     }
 }
 
